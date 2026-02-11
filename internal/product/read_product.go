165bdf3ed7509093c575993451b73d93acd455e2
Ozoniuss <qscxzsewqazxcde@gmail.com>
2026-02-11 22:19:41 +0200

    Detect deactivated ads

--- internal/product/read_product.go
+++ internal/product/read_product.go
@@ -12,6 +12,8 @@ import (
 	"golang.org/x/net/html"
 )
 
+var ErrAdDeactivated = errors.New("olx ad was deactivated")
+
 func FetchProduct(
 	ctx context.Context,
 	client *http.Client,
@@ -29,6 +31,11 @@ func FetchProduct(
 	}
 	defer resp.Body.Close()
 
+	// OLX sends 410 for deactivated ads
+	if resp.StatusCode == http.StatusGone {
+		return nil, ErrAdDeactivated
+	}
+
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("request responded with status %d", resp.StatusCode)
 	}
@@ -37,6 +44,11 @@ func FetchProduct(
 	for {
 		switch z.Next() {
 		case html.ErrorToken:
+			// some AD urls may return a 200 even though the page never existed
+			// and eventually OLX will redirect to a 404 page. But it's written
+			// as a garbage SPA and this redirect is implemented with javascript.
+			// Moreover it will return different error messages depending on
+			// the URL (IDxxx fails differently than simple xxxx)
 			return nil, errors.New("product json-ld not found")
 
 		case html.StartTagToken:
